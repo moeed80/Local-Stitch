76c54f7824c4b3028c511cd43678df3f12ae596b
Moeed Ahmad <email@moeed.com>
2026-05-29 18:54:22 +0300

    Initial Commit

---
+++ PDF Craft/PDF_CraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_CraftApp.swift
+//  PDF Craft
+//
+//  Created by Moeed Ahmad on 5/29/26.
+//
+
+import SwiftUI
+
+@main
+struct PDF_CraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
